e17401cc0de9ca2ca7ed6d01b53dad15b4663a1d
Rob Terrell <Rterrell25@gmail.com>
2020-01-04 12:50:37 -0500

    I have successfully outputted the price of the burger based on ingredients selected

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,9 @@ const controls = [
 
 const BuildControls = props => (
   <div className={classes.BuildControls}>
+    <div>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </div>
     {controls.map(ctrl => (
       <BuildControl
         added={() => props.ingredientAdded(ctrl.type)}
