a773e292afcbde0d3144dd0d9cf9456db48df191
Rob Terrell <Rterrell25@gmail.com>
2020-01-07 11:35:53 -0500

    added logo component and imported it into the toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import burgerLogo from "../../assets/Images/burger-logo.png";
+import classes from "./Logo.module.css";
+
+export default function Logo(props) {
+  return (
+    <div className={classes.Logo}>
+      <img src={burgerLogo} alt="burger" />
+    </div>
+  );
+}
