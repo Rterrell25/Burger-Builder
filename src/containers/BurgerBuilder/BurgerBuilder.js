340a9ebd1c02120880c524ab07cf11dd1e5ad700
Rob Terrell <Rterrell25@gmail.com>
2020-01-28 16:03:55 -0500

    set up async fetching of ingredients in our burger builder action creator and hooked it up to the burger builder container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,6 +18,7 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     console.log(this.props);
+    this.props.onInitIngredients();
   }
 
   updatePurchaseState(ingredients) {
@@ -52,7 +53,11 @@ class BurgerBuilder extends Component {
     }
     let orderSummary = null;
 
-    let burger = <Spinner />;
+    let burger = this.props.error ? (
+      <p>Ingredients can't be fetched</p>
+    ) : (
+      <Spinner />
+    );
 
     if (this.props.ings) {
       burger = (
@@ -94,7 +99,8 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    error: state.error
   };
 };
 const mapDispatchToProps = dispatch => {
@@ -102,7 +108,8 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: ingName =>
       dispatch(burgerBuilderActions.addIngredient(ingName)),
     onIngredientRemoved: ingName =>
-      dispatch(burgerBuilderActions.removeIngredient(ingName))
+      dispatch(burgerBuilderActions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
   };
 };
 
