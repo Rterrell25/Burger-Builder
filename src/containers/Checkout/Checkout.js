3cdec71eccedb92b3812e698c3068128b946b552
Rob Terrell <Rterrell25@gmail.com>
2020-01-31 10:32:07 -0500

    updated the burgerbuilder reducer using utility function

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-import * as actionTypes from "../../store/actions/index";
 
 class Checkout extends Component {
   componentDidMount() {}
