c80ec27447f0b73444b9bedb8b69c76c8477b7dd
Rob Terrell <Rterrell25@gmail.com>
2020-02-05 13:04:47 -0500

    display a log out route if a user is authenticated and give a user the access to authentication route if they have note logged in yet

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -13,7 +13,7 @@ export default function Toolbar(props) {
         <Logo />
       </div>
       <nav className={classes.DesktopOnly}>
-        <NavigationItems />
+        <NavigationItems isAuthenticated={props.isAuth} />
       </nav>
     </header>
   );
